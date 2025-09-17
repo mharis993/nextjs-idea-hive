c8cb04b86f5abe3ed3ec1dd529c5e997c39cbe3c
mharis993 <harismohammad993@gmail.com>
2025-09-17 15:45:01 +0500

    Setup Sanity and created startup card

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,18 @@
 import SearchForm from "@/components/SearchForm";
+import StartupCard from "@/components/StartupCard";
 
 export default async function Home({ searchParams }: { searchParams: Promise<{ query?: string }>; }) {
   const query = (await searchParams).query;
+  const posts = [{
+    _createdAt: new Date(),
+    views: 55,
+    author: { _id: 1, name: 'Haris' },
+    _id: 1,
+    description: 'This is a description',
+    image: 'https://images.unsplash.com/photo-1485827404703-89b55fcc595e?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8cm9ib3RzfGVufDB8fDB8fHww',
+    title: 'The Robots',
+    category: 'Robots',
+  }];
 
   return (
     <>
@@ -17,6 +28,22 @@ export default async function Home({ searchParams }: { searchParams: Promise<{ q
         </p>
 
         <SearchForm query={query} />
+      </section>
+
+      <section className="section_container">
+        <p className="text-30-semibold">
+          {query ? `Search results for "${query}"` : "All Startups"}
+        </p>
+
+        <ul className="mt-7 card_grid">
+          {posts?.length > 0 ? (
+            posts.map((post: StartupTypeCard) => (
+              <StartupCard key={post?._id} post={post} />
+            ))
+          ) : (
+            <p className="no-results">No startups found</p>
+          )}
+        </ul>
       </section>
       <h1 className="heading">Home</h1>
     </>
---
+++ components/StartupCard.tsx
@@ -0,0 +1,81 @@
+import { formatDate } from "@/lib/utils";
+import { EyeIcon } from "lucide-react";
+import Link from "next/link";
+import Image from "next/image";
+import { Button } from "@/components/ui/button";
+// import { Author, Startup } from "@/sanity/types";
+// import { Skeleton } from "@/components/ui/skeleton";
+
+// export type StartupTypeCard = Omit<Startup, "author"> & { author?: Author };
+
+const StartupCard = ({ post }: { post: StartupTypeCard }) => {
+  const {
+    _createdAt,
+    views,
+    author,
+    title,
+    category,
+    _id,
+    image,
+    description,
+  } = post;
+
+  return (
+    <li className="startup-card group">
+      <div className="flex-between">
+        <p className="startup_card_date">{formatDate(_createdAt)}</p>
+        <div className="flex gap-1.5">
+          <EyeIcon className="size-6 text-primary" />
+          <span className="text-16-medium">{views}</span>
+        </div>
+      </div>
+
+      <div className="flex-between mt-5 gap-5">
+        <div className="flex-1">
+          <Link href={`/user/${author?._id}`}>
+            <p className="text-16-medium line-clamp-1">{author?.name}</p>
+          </Link>
+          <Link href={`/startup/${_id}`}>
+            <h3 className="text-26-semibold line-clamp-1">{title}</h3>
+          </Link>
+        </div>
+        <Link href={`/user/${author?._id}`}>
+          <Image
+            src={"https://images.unsplash.com/photo-1563207153-f403bf289096?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8cm9ib3RzfGVufDB8fDB8fHww"}
+            alt={author?.name}
+            width={48}
+            height={48}
+            className="rounded-full"
+          />
+        </Link>
+      </div>
+
+      <Link href={`/startup/${_id}`}>
+        <p className="startup-card_desc">{description}</p>
+
+        <img src={image} alt="placeholder" className="startup-card_img" />
+      </Link>
+
+      <div className="flex-between gap-3 mt-5">
+        <Link href={`/?query=${category?.toLowerCase()}`}>
+          <p className="text-16-medium">{category}</p>
+        </Link>
+        <Button className="startup-card_btn" asChild>
+          <Link href={`/startup/${_id}`}>Details</Link>
+        </Button>
+      </div>
+    </li>
+  );
+};
+
+// export const StartupCardSkeleton = () => (
+//   <>
+//     {[0, 1, 2, 3, 4].map((index: number) => (
+//       <li key={cn("skeleton", index)}>
+//         <Skeleton className="startup-card_skeleton" />
+//       </li>
+//     ))}
+//   </>
+// );
+
+export default StartupCard;
